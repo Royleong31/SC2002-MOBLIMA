2e3f60a65d890485cb50596995908c5b54c57742
iamshlomo <101464061+iamshlomo@users.noreply.github.com>
2022-11-03 01:28:31 +0800

    Update Account.java

--- src/model/Account/Account.java
+++ src/model/Account/Account.java
@@ -4,27 +4,29 @@ package model.Account;
  * Contains the username and password as well as login methods.
  * User is registered upon creation of the account.
  *
- @author Roy Leong
- @version 1.0
+ @author Roy Leong, Ryan Ng
+ @version 1.1
  @since 2022-10-30
 */
-public abstract class Account {
+public class Account {
   /**
    * The username of the account.
+   * There will be no mutator for this.
    */
   private String username;
   
   /**
-   * The password for this account
-   * There is no getter for this as the password should never be able to be accessed out of this class
+   * The password of the account.
+   * There will be no mutator for this.
+   * There is no accessor for this as the password should never be able to be accessed out of this class.
    */
   private String password;
 
   /**
    * Constructor for the Account class.
-   * This is the register method
-   * @param username
-   * @param password
+   * Register method for new Account.
+   * @param username This Account's username.
+   * @param password This Account's password.
    */
   public Account(String username, String password) {
     this.username = username;
@@ -32,17 +34,20 @@ public abstract class Account {
   }
 
   /**
-   * @return username
+   * Gets the username of the account.
+   * @return This account's username.
    */
   public String getUsername() {
     return username;
   }
-
-  /**
-   * 
-   * @param username
-   * @param password
-   * @return the Account object of the logged in user
+ 
+   /**
+   * User authentication method for login process.
+   * @param username This entry of username during login.
+   * @param password This entry of password during login.
+   * @return this The Account object of the logged in user if authentication successful.
+   * @return null value If the login authentication unsuccessful.
+   *		 	  		Can be due to non-existent Account or wrong password.
    */
   public Account login(String username, String password) {
     if (this.username.equals(username) && this.password.equals(password)) {
