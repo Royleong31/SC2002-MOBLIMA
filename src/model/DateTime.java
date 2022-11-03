403bf7c09cce1f14e64331194e3311f225642525
iamshlomo <101464061+iamshlomo@users.noreply.github.com>
2022-11-04 00:01:04 +0800

    Update DateTime.java

--- src/model/DateTime.java
+++ src/model/DateTime.java
@@ -3,6 +3,7 @@ package model;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
+import java.lang.Integer;
 
 /**
  * Realtime date and time formatter
@@ -15,8 +16,8 @@ import java.util.TimeZone;
 public class DateTime {
 
   /**
-   * Formats the date and time(in 24H) to a string.
-   * @return String The formatted date and time.
+   * Formats the date and time(in 24H) to a string
+   * @return String The formatted date and time
    */
   public static String getDateTime() {
     SimpleDateFormat dateForm = new SimpleDateFormat("yyyyMMddHHmm");
@@ -24,4 +25,23 @@ public class DateTime {
     return dateForm.format(new java.util.Date());
   }
   
+  /**
+   * Gets the time in hours.
+   * @return Int The hour in day (0-23).
+   */
+  public static int getTime(){
+    SimpleDateFormat timeForm = new SimpleDateFormat("HH");
+	timeForm.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+	return Integer.parseInt(timeForm.format(new java.util.Date()));
+  }
+  
+  /**
+   * Gets the day of the week.
+   * @return Int The day number of week (1 = Monday, ..., 7 = Sunday).
+   */
+  public static int getDay(){
+    SimpleDateFormat dayForm = new SimpleDateFormat("u");
+	dayForm.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+	return Integer.parseInt(dayForm.format(new java.util.Date()));
+  }
 }
