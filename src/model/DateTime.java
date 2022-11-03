43eb8e1307523db3466d41fdc9c23796da3999fb
iamshlomo <101464061+iamshlomo@users.noreply.github.com>
2022-11-03 22:23:23 +0800

    Update DateTime.java

--- src/model/DateTime.java
+++ src/model/DateTime.java
@@ -4,11 +4,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 
+/**
+ * Realtime date and time formatter
+ * Formats according to Singapore Time, GMT+8
+ *
+ @author Roy Leong, Ryan Ng
+ @version 1.1
+ @since 2022-10-30
+*/
 public class DateTime {
 
   /**
-   * formats the date and time(in 24H) to a string
-   * @return formatted date and time
+   * Formats the date and time(in 24H) to a string.
+   * @return String The formatted date and time.
    */
   public static String getDateTime() {
     SimpleDateFormat dateForm = new SimpleDateFormat("yyyyMMddHHmm");
