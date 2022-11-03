76c29258494e355634935888e6628cbb2c2958a9
icstarslol <i.c.stars.lol@gmail.com>
2022-11-03 01:41:30 -0700

    Updated ReviewManager
    Added new function getReviews to get existing reviews from all movies

--- src/controller/ReviewManager.java
+++ src/controller/ReviewManager.java
@@ -6,15 +6,10 @@ import model.Movie;
 import model.Account.MovieGoerAccount;
 
 public class ReviewManager {
-  private ArrayList<Review> reviews;
-
-  // Not sure I get this but as every review needs a movie, e.g. no review should exist without a movie
-  // So it will be the review manager interacting with the review array list in the movie objects instead of storing its owm
-  public ReviewManager() {}
 
   public void addReview(Review review) throws Exception {
     Movie movie = review.getMovie();
-    reviews = movie.getReviews();
+    ArrayList<Review> reviews = movie.getReviews();
     if (!hasPreviouslySubmittedReview(review.getMovieGoer(), movie)) {
       movie.addReview(review);
     }
@@ -24,15 +19,26 @@ public class ReviewManager {
   }
 
   public boolean hasPreviouslySubmittedReview(MovieGoerAccount movieGoer, Movie movie) {
+    ArrayList<Review> reviews = movie.getReviews();
     Review review;
 
-    for(int i = 0; i < this.reviews.size(); i++) {
-      review = this.reviews.get(i);
+    for(int i = 0; i < reviews.size(); i++) {
+      review = reviews.get(i);
       if(review.getMovieGoer().equals(movieGoer) && review.getMovie().equals(movie)) {
         return true;
       }
     }
 
     return false;
+  }
+
+  public ArrayList<Review> getReviews(ArrayList<Movie> movies) {
+    ArrayList<Review> reviews = new ArrayList<Review>();
+
+    for (Movie movie : movies) {
+      reviews.addAll(movie.getReviews());
+    }
+
+    return reviews;
   }
 }
