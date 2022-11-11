8f3145c999870de3e97592646144aacf0b11d977
auglxw <augustineleexuanwei@gmail.com>
2022-11-11 23:20:56 +0800

    edited javadoc

--- src/controller/CineplexManager.java
+++ src/controller/CineplexManager.java
@@ -16,9 +16,21 @@ import model.SeatingPlan;
  @since 2022-10-30
 */
 public class CineplexManager {
+  /**
+   * Collection of all cineplexes the company owns
+   */
   private ArrayList<Cineplex> cineplexesArr = new ArrayList<Cineplex>();
+  
+  /**
+   * Collection of all cinema halls the company owns
+   */
   private ArrayList<Cinema> cinemasArr = new ArrayList<Cinema>();
 
+  /**
+   * Add new cineplex
+   * @param location
+   * @throws Exception if there already exists a cineplex in the same location
+   */
   public void addCineplex(String location) throws Exception {
     for (Cineplex cur: this.cineplexesArr) {
       if (cur.getLocation().equals(location)) {
@@ -30,6 +42,16 @@ public class CineplexManager {
     this.cineplexesArr.add(new Cineplex(location));
   }
 
+  /**
+   * Add new cinema hall in a particular cineplex
+   * @param cineplex
+   * @param rows
+   * @param columns
+   * @param aisle
+   * @param cinemaType
+   * @return new cinema object
+   * @throws Exception if cinema already exists
+   */
   public Cinema addCinema(Cineplex cineplex, int rows, int columns, int aisle, CinemaType cinemaType) throws Exception {
     SeatingPlan seatingPlan = new SeatingPlan(rows, columns, aisle);
     Cinema cinema = new Cinema(seatingPlan, cineplex, cinemaType);
@@ -38,6 +60,12 @@ public class CineplexManager {
     return cinema;
   }
 
+  /**
+   * Get a particular cinema object from its ID
+   * @param cinemaId
+   * @return cinema object
+   * @throws Exception if ID does not link to any cinema does not exist
+   */
   public Cinema getCinemaById(String cinemaId) throws Exception {
     for (Cinema cur: this.cinemasArr) {
       if (cur.getId().equals(cinemaId)) {
@@ -48,6 +76,12 @@ public class CineplexManager {
     throw new Exception("A cinema with the specified ID does not exist.");
   }
 
+  /**
+   * Get cineplex based on its location
+   * @param cineplexLocation
+   * @return cineplex object
+   * @throws Exception if there is no cineplex at that location
+   */
   public Cineplex getCineplexByLocation(String cineplexLocation) throws Exception {
     for (Cineplex cur: this.cineplexesArr) {
       if (cur.getLocation().equals(cineplexLocation)) {
@@ -58,15 +92,30 @@ public class CineplexManager {
     throw new Exception("A cineplex with the specified location does not exist.");
   }
 
-  // add delete method for cineplex?
+  /**
+   * Delete a particular cinema hall
+   * @param cineplex
+   * @param cinema
+   * @throws Exception if that cinema does not exist in that cineplex
+   */
   public void deleteCinema(Cineplex cineplex, Cinema cinema) throws Exception {
     cineplex.deleteCinema(cinema);
   }
 
+  /**
+   * Get an arraylist of all cineplexes
+   * @return
+   */
   public ArrayList<Cineplex> getCineplexes() {
     return this.cineplexesArr;
   }
 
+  /**
+   * Get all cinema halls of a cineplex
+   * @param cineplex
+   * @return
+   * @throws Exception if the cineplex does not exist
+   */
   public ArrayList<Cinema> getCinemas(Cineplex cineplex) throws Exception {
     /* Check if cineplex exists */
     for (Cineplex cur: this.cineplexesArr) {
--- src/controller/SystemManager.java
+++ src/controller/SystemManager.java
@@ -16,45 +16,78 @@ enum SpecialDay {
 }
 
 /**
- * Account for a staff member.
- * Contains the staff id
+ * System Manager
+ * Manages all system data (holidays, price multipliers and sorting criteria)
  *
  @author Roy Leong, Augustine Lee
  @version 1.1
  @since 2022-10-30
 */
 public class SystemManager {
+  /**
+   * Collection of holidays in the system
+   */
   private ArrayList<DateTime> holidaysArr = new ArrayList<DateTime>();
+
+  /**
+   * Hashmap that maps each cinema type to a price multiplier
+   */
   private HashMap<CinemaType, Float> cinemaMultMap = Constants.DEFAULT_CINEMA_PRICE_MAP;
+
+  /**
+   * Hashmap that maps each seat type to a price multiplier
+   */
   private HashMap<SeatType, Float> seatMultMap = Constants.DEFAULT_SEAT_PRICE_MAP;
+  
+  /**
+   * Sorting criteria set for sorting top movies
+   */
   private SortCriteria movieSortingCriteria = Constants.DEFAULT_SORT_CRITERIA;
 
+  /**
+   * Get all holiday dates stored in the system
+   * 
+   * @return arraylist of holidays
+   */
   public ArrayList<DateTime> getHolidays() {
     return holidaysArr;
   }
 
-  /*
-   * @param CinemaType
-   * @return Float The multiplier for the cinema type
-   */
+   /**
+    * Get the price multiplier for a particular cinema type
+    *
+    * @param ct cinema type (enum)
+    * @return price multiplier
+    */
   public float getCinemaMultiplier(CinemaType ct) {
     return this.cinemaMultMap.get(ct);
   }
 
-  // TODO: Add authorisation decorators for all admin methods. Do the same for other methods in other managers
-  /*
-   * @param CinemaType
-   * @param Float The multiplier to set for the cinema type
-   * @return void
+  /**
+   * Set a new price multiplier for a particular cinema type
+   * 
+   * @param ct the target cinema type (enum)
+   * @param mult the new price multiplier
    */
   public void setCinemaMultiplier(CinemaType ct, float mult) {
     this.cinemaMultMap.put(ct, mult);
   }
   
+  /**
+   * Get the current sorting criteria setting
+   * 
+   * @return current sorting criteria (enum)
+   */
   public SortCriteria getSortingCriteria() {
     return this.movieSortingCriteria;
   }
   
+  /**
+   * Set a new sorting criteria to be the system's default sorting criteria for sorting top movies
+   * 
+   * @param sc new sorting criteria (enum)
+   * @throws Exception if there is an invalid sorting criteria (not sales or rating) passed in
+   */
   public void setSortingCriteria(SortCriteria sc) throws Exception {
     // These are the only 2 sorting criteria allowed
     if (sc.equals(SortCriteria.SALES) || sc.equals(SortCriteria.RATING)) {
@@ -64,11 +97,12 @@ public class SystemManager {
     throw new Exception("Invalid sorting criteria");
   }
   
-  /*
-  * @param CinemaType
-  * @return void
-  */
-  // TODO: Can't we set it to 1 instead? Why do we need to remove it?
+  /**
+   * Delete a particular cinema type's multiplier
+   * 
+   * @param ct cinema type (enum)
+   * @throws Exception if the particular cinema type does not have a multiplier
+   */
   public void deleteCinemaMultiplier(CinemaType ct) throws Exception {
     if (cinemaMultMap.containsKey(ct)) {
       cinemaMultMap.remove(ct);
@@ -76,28 +110,32 @@ public class SystemManager {
     throw new Exception("Cinema type do not exists.");
   }
 
-  /*
-   * @param SeatType
-   * @return Float The multiplier for the seat type
+  /**
+   * Get the price multiplier for a seat type
+   * 
+   * @param st seat type (enum)
+   * @return price multiplier for that seat type
    */
   public float getSeatMultiplier(SeatType st) {
     return seatMultMap.get(st);
   }
 
-  /*
-   * @param SeatType
-   * @param Float The multiplier to set for the seat type
-   * @return void
+  /**
+   * Set a new price multiplier for a particular seat type
+   * 
+   * @param st seat type(enum)
+   * @param multiplier new price multiplier
    */
   public void setSeatMultiplier(SeatType st, float multiplier) {
     seatMultMap.put(st, multiplier);
   }
   
-  /*
-  * @param SeatType
-  * @return void
-  */
-  // TODO: Can't we set it to 1 instead? Why do we need to remove it?
+  /**
+   * Delete a price multiplier for a seat type
+   * 
+   * @param st seat type (enum)
+   * @throws Exception if seat type does not exist in hashmap
+   */
   public void deleteSeatMultiplier(SeatType st) throws Exception {
     if (seatMultMap.containsKey(st)) {
       seatMultMap.remove(st);
@@ -105,13 +143,14 @@ public class SystemManager {
     throw new Exception("Seat type does not exists.");
   }
 
-  /*
-   * @param Int year
-   * @param Int month
-   * @param Int day
-   * @return void
+  /**
+   * Delete a holiday date from system's collection of holidays
+   * 
+   * @param year
+   * @param month
+   * @param day
+   * @throws Exception if holiday does not exist in the system collection
    */
-  // !: Needs to be in 2-digit format
   public void deleteHoliday(int year, int month, int day) throws Exception {
     DateTime dt = new DateTime(year, month, day);
     
@@ -122,11 +161,15 @@ public class SystemManager {
     this.holidaysArr.removeIf(value -> dt.isDayEqual(value));
   }
 
-  /*
-   * @param Int year
-   * @param Int month
-   * @param Int day
-   * @return void
+  /**
+   * Add new holiday to the system's collection of holiday
+   * 
+   * @param year
+   * @param month
+   * @param day
+   * @param hour
+   * @param minute
+   * @throws Exception if holiday already exists in the system's collection
    */
   public void addHoliday(int year, int month, int day, int hour, int minute) throws Exception {
     DateTime date = new DateTime(year, month, day, hour, minute);
@@ -137,6 +180,12 @@ public class SystemManager {
     this.holidaysArr.add(date);
   }
 
+  /**
+   * Check if a particular date is a holiday
+   * 
+   * @param date
+   * @return
+   */
   public boolean isHoliday(DateTime date) {
     boolean containsDate = false;
 
