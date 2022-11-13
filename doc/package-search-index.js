00f5fc3d2504fb0911a11386a4c5a8b1dec82961
Roy Leong <l3ongroy@gmail.com>
2022-11-13 10:54:57 +0800

    add html javadoc

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"constants"},{"l":"controller"},{"l":"enums"},{"l":"model"},{"l":"model.Account"},{"l":"utils"},{"l":"view"}];updateSearchResults();
